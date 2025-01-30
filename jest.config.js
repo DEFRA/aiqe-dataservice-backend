a6ceec24a6a65b007b80f796b08a21f6bdb27b8b
Karthick Muthukrishnan <karthick.muthukrishnan@cognizant.com>
2025-01-30 11:46:52 +0000

    adp-176
    Error fixing the deployment issue on import file to include .Js extension

--- jest.config.js
+++ jest.config.js
@@ -19,7 +19,16 @@ export default {
     '<rootDir>/.server',
     'index.js'
   ],
-  coverageDirectory: '<rootDir>/coverage'
+  coverageDirectory: '<rootDir>/coverage',
+  transform: {
+    '^.+\\.js$': 'babel-jest'
+  },
+  transformIgnorePatterns: [
+    `node_modules/(?!${[
+      '@defra/hapi-tracing', // Supports ESM only
+      'node-fetch' // Supports ESM only
+    ].join('|')}/)`
+  ]
 }
 
 /**
